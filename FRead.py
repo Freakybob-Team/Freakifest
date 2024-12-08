8856e80e20e5426aea7ae0352466bde8bcead599
Wish <100239673+wish13yt@users.noreply.github.com>
2024-12-07 18:00:58 -0600

    modified:   FRead.py

--- FRead.py
+++ FRead.py
@@ -3,4 +3,4 @@ with open('F.json', 'r') as file:
     data = json.load(file)
     print("Freakifest Version: " + data['f_ver'])
     print("Is Greg: " + data['isGreg'])
-    file.close
+    file.close()

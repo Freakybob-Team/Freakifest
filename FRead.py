3891f6dd77d7bb9e988e6dbf4b74744a14d48ac0
Wish <100239673+wish13yt@users.noreply.github.com>
2024-12-07 18:25:11 -0600

    modified:   F.json modified:   FRead.py

--- FRead.py
+++ FRead.py
@@ -1,13 +1,18 @@
 import json
+print("Only approved Cus will show.")
 with open('F.json', 'r') as file:
     data = json.load(file)
     print("Freakifest Version: " + data['f_ver'])
-    print("Is Greg: " + data['isGreg'])
+    # print("Is Greg: " + data['isGreg'])
     print("Name: " + data['name'])
     print("Software Version: " + data['soft_ver'])
     print("Description: " + data['description'])
     try:
         print("Rating (site.freakybob): " + data['site.freakybob.ratings'])
+    except NameError:
+        None
+    try:
+        print("Website (via " + data['website.provider'] + "): " + data['website'])
     except NameError:
         None
     file.close()

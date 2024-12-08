9f1b3fe647ffdac7108fd25128b800ff4d773fda
Wish <100239673+wish13yt@users.noreply.github.com>
2024-12-07 18:03:52 -0600

    lease enter the commit message for your changes. Lines starting ith '#' will be ignored, and an empty message aborts the commit.
    n branch main
    our branch is up to date with 'origin/main'.
    
    hanges to be committed:
    modified:   F.json
    modified:   FRead.py

--- FRead.py
+++ FRead.py
@@ -3,4 +3,7 @@ with open('F.json', 'r') as file:
     data = json.load(file)
     print("Freakifest Version: " + data['f_ver'])
     print("Is Greg: " + data['isGreg'])
+    print("Name: " + data['name'])
+    print("Software Version: " + data['soft_ver'])
+    print("Description: " + data['description'])
     file.close()
